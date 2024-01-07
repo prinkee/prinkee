8e606483fd36fc344c3c28b133449906e6368f51
Kyoku <prinkeee@proton.me>
2024-01-07 14:30:32 -0500

    update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home() {
           name="keywords"
           content="Full Stack Developer, Software Engineer, Projects, Skills, Collaborations, Job Opportunities"
         />
+        <meta property="image" content="/favicon.png" />
         <meta name="author" content="prinke" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
